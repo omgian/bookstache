8ae70beace2fe20c2eeafeb70472a25d64c95446
Yolanda Haynes <yhaynes@giyo.us>
2021-06-15 10:31:26 -0700

    added delete feature on modal

--- public/js/feed.js
+++ public/js/feed.js
@@ -21,8 +21,15 @@ async function toggleFavorite(id) {
         console.log(err)
     }
 }
+function prepareBookForDelete(book) {
+    const title = document.getElementById('deleteBookTitle');
+    title.innerHTML = book.title;
+    const bookId = document.getElementById('deleteBookId');
+    bookId.innerHTML = book.id;
+}
+async function deleteBook() {
 
-async function deleteBook(id) {
+    const id = document.getElementById('deleteBookId').innerHTML;
     try {
         const response = await fetch(`book/deleteBook?id=${id}`, {
             method: 'DELETE',
